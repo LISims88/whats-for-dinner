ea38e147b37da28ea0fb0897d104c97770213d4a
Lydia Sims <sims.lydia88@gmail.com>
2023-12-22 18:57:35 -0600

    Update the CSS and HTML and the JS files.

---
+++ main.js
@@ -0,0 +1,98 @@
+var sides = [
+    'Broccoli Salad',
+    'Miso Glazed Carrots',
+    'Coleslaw',
+    'Corn Salad',
+    'Fried Rice',
+    'Garden Salad',
+    'Crispy Potatoes',
+    'Hasselback Potatoes',
+    'Sweet Potato Tots',
+    'Mfarakeh',
+    'Coconut Rice',
+    'Caeser Pasta Salad',
+    'Shrimp Summer Rolls',
+    'Garlic Butter Mushrooms',
+    'Hush Puppies',
+    'Saka Saka',
+    'Kamounia',
+    'Maklouba'
+]
+
+var  mains =[
+    'Spaghetti and Meatballs',
+    'Pineapple Chicken',
+    'Shakshuka',
+    'Thai Yellow Curry',
+    'Bibimbap',
+    'Chicken and Sausage Gumbo',
+    'Chicken Parmesean',
+    'Butternut Squash Soup',
+    'BBQ Chicken Burgers',
+    'Tonkotsu Ramen',
+    'Empanadas',
+    'Chicken Fried Rice',
+    'Sheet Pan Fajitas',
+    'Margarita Pizza',
+    'Shwarma',
+    'Mussakhan',
+    'Soso',
+    'Shaiyah',
+]
+
+var desserts = [
+    'Apple Pie',
+    'Lemon Meringue Pie',
+    'Banana Pudding',
+    'Peach Cobbler',
+    'Cheesecake',
+    'Funfetti Cake',
+    'Baklava',
+    'Flan',
+    'Macarons',
+    'Chocolate Cupcakes',
+    'Pavlova',
+    'Pumpkin Pie',
+    'Tart Tatin',
+    'Croissants',
+    'Eclairs',
+    'Knafeh',
+    'Caakiri',
+    'Kahk'
+]
+
+var letsCookButton = document.getElementById("letsCookButton");
+var recipeResults = document.querySelector('.recipe-results')
+var recipePending = document.querySelector('.recipe-pending')
+var selectedRecipeType;
+
+letsCookButton.addEventListener("click", function() {
+    selectedRecipeType = document.querySelector('input[name="recipe-type"]:checked');
+    if (selectedRecipeType) {
+        getRandomDish();
+    }
+});
+
+ function getRandomSide() {
+    var randomSide = sides[Math.floor(Math.random() * sides.length)]
+    return randomSide
+ }
+
+ function getRandomMain(){
+    var randomMain = mains[Math.floor(Math.random() * mains.length)]
+    return randomMain
+ }
+
+ function getRandomDessert(){
+    var randomDessert = desserts[Math.floor(Math.random() * desserts.length)]
+    return randomDessert
+ }
+
+ function getRandomDish(){
+    var randomDish = getRandomSide() || getRandomMain() || getRandomDessert();
+   
+    recipeResults.querySelector('p').innerText = `${randomDish}`
+    
+    recipeResults.style.display = 'block'
+    recipePending.style.display = 'none'
+ }
